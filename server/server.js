059aaa9fd3c79345d4b421b030857b980ca04049
cchelton <59346941+cchelton@users.noreply.github.com>
2020-03-28 11:58:09 -0500

    imports, routes
    imported the coleMath module and the history module
    routed history to /history

--- server/server.js
+++ server/server.js
@@ -5,6 +5,25 @@ const app = express();
 
 const PORT = 5000;
 
+//
+// MODULES
+//
+
+const history = require("./modules/history"); //  [ { mathString: '', result: 0 } ]
+
+const coleMath = require("./modules/coleMath"); //  my math object
+
+//
+// ROUTES
+//
+
+app.get("/history", (req, res) => {
+  res.send(history);
+});
+
+//
+// LISTEN
+//
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
 });
