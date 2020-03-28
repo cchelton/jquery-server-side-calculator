21f4f5376252308f919421a4ac73762f190c98d0
cchelton <59346941+cchelton@users.noreply.github.com>
2020-03-28 12:51:52 -0500

    routed calculations
    added the addition, subtraction, multiplication, and division routes.
    
    these routes calculate the result, change the current result in the server and append the calculation to history.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const coleMath = require("./modules/coleMath"); //  my math object
 
 const app = express();
 
 const PORT = 5000;
 
 //
-// MODULES
+// VARIABLES
 //
 
 const history = require("./modules/history"); //  [ { mathString: '', result: 0 } ]
-
-const coleMath = require("./modules/coleMath"); //  my math object
+let result = 0;
 
 //
 // ROUTES
@@ -26,6 +26,7 @@ app.get("/history", (req, res) => {
 });
 
 app.post("/history", (req, res) => {
+  // testing route for me     TODO: remove this route
   const newCalc = req.body; //  format { mathString: '', result: 0 }
   console.log(newCalc);
   history.push(newCalc);
@@ -33,9 +34,66 @@ app.post("/history", (req, res) => {
   res.sendStatus(201);
 });
 
+app.post("/add", (req, res) => {
+  //  perform addition and append history
+  const [num1, num2] = req.body; // unpack data. data format [num1, num2]
+  const newMathString = `${num1}+${num2}`; //  prep for history
+  const newResult = coleMath.add(num1, num2); //  calculate result
+
+  result = newResult; //  change current result
+
+  const newHistoryObject = coleMath.newHistObj(newMathString, newResult);
+  history.push(newHistoryObject); //  append history w/ new calculation
+
+  res.send(201);
+});
+
+app.post("/subtract", (req, res) => {
+  //  perform subtraction and append history
+  const [num1, num2] = req.body; // unpack data. data format [num1, num2]
+  const newMathString = `${num1}-${num2}`; //  prep for history
+  const newResult = coleMath.sub(num1, num2); //  calculate result
+
+  result = newResult; //  change current result
+
+  const newHistoryObject = coleMath.newHistObj(newMathString, newResult);
+  history.push(newHistoryObject); //  append history w/ new calculation
+
+  res.send(201);
+});
+
+app.post("/multiply", (req, res) => {
+  //  perform multiplication and append history
+  const [num1, num2] = req.body; // unpack data. data format [num1, num2]
+  const newMathString = `${num1}*${num2}`; //  prep for history
+  const newResult = coleMath.mul(num1, num2); //  calculate result
+
+  result = newResult; //  change current result
+
+  const newHistoryObject = coleMath.newHistObj(newMathString, newResult);
+  history.push(newHistoryObject); //  append history w/ new calculation
+
+  res.send(201);
+});
+
+app.post("/divide", (req, res) => {
+  //  perform division and append history
+  const [num1, num2] = req.body; // unpack data. data format [num1, num2]
+  const newMathString = `${num1}/${num2}`; //  prep for history
+  const newResult = coleMath.div(num1, num2); //  calculate result
+
+  result = newResult; //  change current result
+
+  const newHistoryObject = coleMath.newHistObj(newMathString, newResult);
+  history.push(newHistoryObject); //  append history w/ new calculation
+
+  res.send(201);
+});
+
 //
 // LISTEN
 //
+
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
 });
