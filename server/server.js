e77f161c943513e70d8ecebaf7deeb2280e84319
cchelton <59346941+cchelton@users.noreply.github.com>
2020-03-28 12:14:28 -0500

    added static route and bodyParser
    forgot to setup body parser properly, added necessary lines.
    
    added static route to public directory

--- server/server.js
+++ server/server.js
@@ -17,8 +17,20 @@ const coleMath = require("./modules/coleMath"); //  my math object
 // ROUTES
 //
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("server/public"));
+
 app.get("/history", (req, res) => {
   res.send(history);
+});
+
+app.post("/history", (req, res) => {
+  const newCalc = req.body; //  format { mathString: '', result: 0 }
+  console.log(newCalc);
+  history.push(newCalc);
+
+  res.sendStatus(201);
 });
 
 //
