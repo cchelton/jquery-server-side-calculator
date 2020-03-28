6c9efc68313f07edd5db6b739900c8e3338eb11c
cchelton <59346941+cchelton@users.noreply.github.com>
2020-03-28 11:54:25 -0500

    created module

---
+++ server/modules/coleMath.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /**
+   * Adds 2 numbers.
+   * @param     {number}    a   addend
+   * @param     {number}    b   addend
+   * @returns   {number}        sum
+   */
+  add(a, b) {
+    return a + b;
+  },
+
+  /**
+   * Subtracts 2 numbers.
+   * @param     {number}    a   minuend
+   * @param     {number}    b   subtrahend
+   * @returns   {number}        difference
+   */
+  sub(a, b) {
+    return a - b;
+  },
+
+  /**
+   * Divides 2 numbers.
+   * @param     {number}    a   dividend
+   * @param     {number}    b   divisor
+   * @returns   {number}        quotient
+   */
+  div(a, b) {
+    return a / b;
+  },
+
+  /**
+   * Multiplies 2 numbers
+   * @param     {number}    a   factor
+   * @param     {number}    b   factor
+   * @returns   {number}        product
+   */
+  mul(a, b) {
+    return a * b;
+  }
+};
