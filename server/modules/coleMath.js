fc61d9fdecf7bf5f079e0ae9f63978b559c42a97
cchelton <59346941+cchelton@users.noreply.github.com>
2020-03-28 12:41:35 -0500

    added newHistObj method

--- server/modules/coleMath.js
+++ server/modules/coleMath.js
@@ -3,7 +3,7 @@ module.exports = {
    * Adds 2 numbers.
    * @param     {number}    a   addend
    * @param     {number}    b   addend
-   * @returns   {number}        sum
+   * @returns                   sum
    */
   add(a, b) {
     return a + b;
@@ -13,7 +13,7 @@ module.exports = {
    * Subtracts 2 numbers.
    * @param     {number}    a   minuend
    * @param     {number}    b   subtrahend
-   * @returns   {number}        difference
+   * @returns                   difference
    */
   sub(a, b) {
     return a - b;
@@ -23,19 +23,32 @@ module.exports = {
    * Divides 2 numbers.
    * @param     {number}    a   dividend
    * @param     {number}    b   divisor
-   * @returns   {number}        quotient
+   * @returns                   quotient
    */
   div(a, b) {
     return a / b;
   },
 
   /**
-   * Multiplies 2 numbers
+   * Multiplies 2 numbers.
    * @param     {number}    a   factor
    * @param     {number}    b   factor
-   * @returns   {number}        product
+   * @returns                   product
    */
   mul(a, b) {
     return a * b;
+  },
+
+  /**
+   * Prepares a new history object to be pushed to the array.
+   * @param {string} mathStr the mathString for a history obj ex. "1+2"
+   * @param {number} mathResult the result of calculations
+   * @returns object ready to be pushed to history
+   */
+  newHistObj(mathStr, mathResult) {
+    return {
+      mathString: mathStr,
+      result: mathResult
+    };
   }
 };
